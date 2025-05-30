282e31262b425cbcd970bc39fc3d563394c66a51
Ahamed Basha <ahamedbsha436@gmail.com>
2025-05-30 22:18:12 +0530

    Food Delivery App Using React.JS

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className='header-content'>
+         <h2>Order Your Favorite Food Here</h2>
+         <p>A Food Delivery App makes ordering food easy and convenient by connecting customers with nearby restaurants. Users can browse a wide range of cuisines, add their favorite dishes to the cart, place an order, and get it delivered right.</p>
+         <button>View menu</button>
+       </div>
+    </div>
+  )
+}
+
+export default Header
